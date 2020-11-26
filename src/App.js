f901485cf96cd9a201f5e059b435d8d6d412de31
DioniMF <dionimf@unesc.net>
2020-11-26 19:50:47 -0300

    finalizado desafio conseitos reactjs

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
-import React from "react";
-
+import React, { useState, useEffect} from "react";
+import api from "./services/api";
 import "./styles.css";
 
 function App() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(()=>{
+    api.get('repositories').then(res=>{
+      setRepositories(res.data);
+    })
+  }, []);
+
   async function handleAddRepository() {
+    const res = await api.post('repositories', { 
+      title: 'Teste',
+      url: 'https://github.com/dionimf',
+      techs: ['node', 'python']
+    });
+
+    setRepositories([...repositories, res.data])
     // TODO
   }
 
   async function handleRemoveRepository(id) {
+    await api.delete(`repositories/${id}`);
+    setRepositories(repositories.filter(
+      repo => repo.id !== id
+    ));
     // TODO
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
+        {repositories.map(repo => (
+          <li key={repo.id}>
+            {repo.title}
+          <button onClick={() => handleRemoveRepository(repo.id)}>
             Remover
           </button>
         </li>
+        ))}
+        
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
